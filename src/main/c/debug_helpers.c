f57254ce13ae3c1f60e7872f67b290d772aef2ce
Gustavo Lopes <mail@geleia.net>
2021-05-21 10:44:39 +0100

    MSVC support

--- src/main/c/debug_helpers.c
+++ src/main/c/debug_helpers.c
@@ -15,6 +15,8 @@ typedef struct {
     size_t offset;
 } hstring;
 
+#define INITIAL_CAPACITY ((size_t)16)
+
 static void _hstring_write_pwargs(hstring *str, size_t depth,
                                   const PWArgs *pwargs);
 
@@ -34,19 +36,35 @@ JNIEXPORT jstring JNICALL Java_io_sqreen_powerwaf_Powerwaf_pwArgsBufferToJson(
 
     PWArgs root;
     memcpy(&root, input_p, sizeof root);
-    hstring str = {0};
+    hstring str = {
+        .buffer = malloc(INITIAL_CAPACITY),
+        .capacity = INITIAL_CAPACITY
+    };
+    if (!str.buffer) {
+        return NULL;
+    }
     _hstring_write_pwargs(&str, 0, &root);
+#ifdef __clang_analyzer__
+    // due to other exclusions, analyzer doesn't know str.buffer was written
+    jstring jstr = NULL;
+#else
     jstring jstr = java_utf8_to_jstring_checked(env, str.buffer, str.offset);
+#endif
     free(str.buffer);
     return jstr;
 }
 
-#define max(a, b)                                                              \
+#if defined(__GNUC__) || defined(__clang__)
+# define max(a, b)                                                              \
     ({                                                                         \
         __typeof__(a) _a = (a);                                                \
         __typeof__(b) _b = (b);                                                \
         _a > _b ? _a : _b;                                                     \
     })
+#else
+// this evaluates a and b twice though
+# define max(a, b)  (((a) > (b)) ? (a) : (b))
+#endif
 
 static bool _hstring_ensure_extra_capacity(hstring *str, size_t data_size)
 {
@@ -56,7 +74,11 @@ static bool _hstring_ensure_extra_capacity(hstring *str, size_t data_size)
         if (new_capacity < str->capacity) { // wrap-around
             return false;
         }
-        str->buffer = realloc(str->buffer, new_capacity);
+        char *new_buffer = realloc(str->buffer, new_capacity);
+        if (!new_buffer) {
+            return false;
+        }
+        str->buffer = new_buffer;
         str->capacity = new_capacity;
     }
     return true;
@@ -64,10 +86,16 @@ static bool _hstring_ensure_extra_capacity(hstring *str, size_t data_size)
 }
 static void _hstring_append(hstring *str, const char *data, size_t data_size)
 {
+    if (data_size == 0) {
+        return;
+    }
     if (!_hstring_ensure_extra_capacity(str, data_size)) {
         return;
     }
+#ifndef __clang_analyzer__
+    // clang analyzer doesn't seem to look into ensure_extra_capacity
     memcpy(str->buffer + str->offset, data, data_size);
+#endif
     str->offset += data_size;
 }
 #define HSTRING_APPEND_CONST(str, constant) \
@@ -75,11 +103,17 @@ static void _hstring_append(hstring *str, const char *data, size_t data_size)
 
 static void _hstring_repeat(hstring *str, char c, size_t repeat_times)
 {
+    if (repeat_times == 0) {
+        return;
+    }
     if (!_hstring_ensure_extra_capacity(str, repeat_times)) {
         return;
     }
     for (size_t i = 0; i < repeat_times; i++) {
+#ifndef __clang_analyzer__
+    // clang analyzer doesn't seem to look into ensure_extra_capacity
         str->buffer[str->offset + i] = c;
+#endif
     }
     str->offset += repeat_times;
 }
@@ -103,16 +137,22 @@ static void _hstring_write_pwargs(hstring *str, size_t depth,
     case PWI_SIGNED_NUMBER: {
         HSTRING_APPEND_CONST(str, "<SIGNED> ");
         char scratch[sizeof("-9223372036854775808")];
-        int len = sprintf(scratch, "%" PRId64, pwargs->intValue);
-        _hstring_append(str, scratch, (size_t) len);
+        int len = snprintf(scratch, sizeof(scratch), "%" PRId64,
+                           pwargs->intValue);
+        if (len < sizeof scratch) {
+            _hstring_append(str, scratch, (size_t) len);
+        } // else should never happen
         HSTRING_APPEND_CONST(str, "\n");
         break;
     }
     case PWI_UNSIGNED_NUMBER: {
         HSTRING_APPEND_CONST(str, "<UNSIGNED> ");
         char scratch[sizeof("18446744073709551615")];
-        int len = sprintf(scratch, "%" PRIu64, pwargs->uintValue);
-        _hstring_append(str, scratch, (size_t) len);
+        int len = snprintf(scratch, sizeof(scratch), "%" PRIu64,
+                           pwargs->uintValue);
+        if (len < sizeof scratch) {
+            _hstring_append(str, scratch, (size_t) len);
+        } // else should never happen
         HSTRING_APPEND_CONST(str, "\n");
         break;
     }
