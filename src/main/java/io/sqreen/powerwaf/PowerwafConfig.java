271a71054e7a8c9c7ef0793c0e71a435ef07c1d2
Santiago M. Mola <santiago.mola@datadoghq.com>
2024-08-08 16:57:20 +0200

    Update sensitive data redaction regexes (#128)

--- src/main/java/io/sqreen/powerwaf/PowerwafConfig.java
+++ src/main/java/io/sqreen/powerwaf/PowerwafConfig.java
@@ -10,10 +10,9 @@ package io.sqreen.powerwaf;
 
 public class PowerwafConfig {
     private final static String DEFAULT_KEY_REGEX =
-            "(?i)(p(ass)?w(or)?d|pass(_?phrase)?|secret|(api_?|private_?|" +
-                    "public_?)key)|token|consumer_?(id|key|secret)|sign(ed|ature)|bearer|authorization";
+            "(?i)pass|pw(?:or)?d|secret|(?:api|private|public|access)[_-]?key|token|consumer[_-]?(?:id|key|secret)|sign(?:ed|ature)|bearer|authorization|jsessionid|phpsessid|asp\\.net[_-]sessionid|sid|jwt";
 
-    private final static String DEFAULT_VALUE_REGEX = "";
+    private final static String DEFAULT_VALUE_REGEX = "(?i)(?:p(?:ass)?w(?:or)?d|pass(?:[_-]?phrase)?|secret(?:[_-]?key)?|(?:(?:api|private|public|access)[_-]?)key(?:[_-]?id)?|(?:(?:auth|access|id|refresh)[_-]?)?token|consumer[_-]?(?:id|key|secret)|sign(?:ed|ature)?|auth(?:entication|orization)?|jsessionid|phpsessid|asp\\.net(?:[_-]|-)sessionid|sid|jwt)(?:\\s*=[^;]|\"\\s*:\\s*\"[^\"]+\")|bearer\\s+[a-z0-9\\._\\-]+|token:[a-z0-9]{13}|gh[opsu]_[0-9a-zA-Z]{36}|ey[I-L][\\w=-]+\\.ey[I-L][\\w=-]+(?:\\.[\\w.+\\/=-]+)?|[\\-]{5}BEGIN[a-z\\s]+PRIVATE\\sKEY[\\-]{5}[^\\-]+[\\-]{5}END[a-z\\s]+PRIVATE\\sKEY|ssh-rsa\\s*[a-z0-9\\/\\.+]{100,}";
 
     public final static PowerwafConfig DEFAULT_CONFIG = new PowerwafConfig();
 
